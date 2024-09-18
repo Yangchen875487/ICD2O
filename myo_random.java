b4364c8b2a92fe52a6ff98d05da083f18566781f
Yangchen875487 <875487@pdsb.net>
2024-09-18 14:05:43 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -1,2 +1,9 @@
-class myo_random [
+class myo_random [class Testing {
+  public static void main(String [] args) {
+    // place your code here!
+    String name = "Yangchen"; 
+    System.out.println("Hello " + name);
+    // your code ends here!
+  }
+}
 ]
