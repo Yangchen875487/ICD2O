48b7ed82097c97bf020195b876fb5a64edf5bbfc
Yangchen875487 <875487@pdsb.net>
2024-09-20 12:46:47 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -17,7 +17,13 @@ System.out.println(" cookies for each bowl.");
 System.out.print("Sanjay has "+ 6 + " for " + 144 + " cookies.");
 System.out.println("The number of cookies per bowl is");
 System.out.println ("The numbers of cookies per bowl is: " + (144/6));
+    System.out.println(24);
+        int amount = 24;
+        System.out.print(amount);
+        System.out.println(" Two dozen is");
+        System.out.println(amount);
     // your code ends here!
+    
   }
 }
 ]
