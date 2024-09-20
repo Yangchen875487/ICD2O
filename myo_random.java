71618da3f492f1b15ff388b36f5e688abbba4d64
Yangchen875487 <875487@pdsb.net>
2024-09-19 22:47:18 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -3,6 +3,10 @@ class myo_random [class Testing {
     // place your code here!
     String name = "Yangchen"; 
     System.out.println("Hello " + name);
+     System.out.println("The number of keys on a keyboard is");
+  System.out.println(102);
+  System.out.println("The value of pi is");
+  System.out.println(3.14159);
     // your code ends here!
   }
 }
