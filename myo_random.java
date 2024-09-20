93c8f1c7b2d1a465a2a61424bcc00978c9b29924
Yangchen875487 <875487@pdsb.net>
2024-09-19 22:08:38 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -3,6 +3,11 @@ class myo_random [class Testing {
     // place your code here!
     String name = "Yangchen"; 
     System.out.println("Hello " + name);
+    System.out.println("What mammal has the most teeth?");
+System.out.print("The Giant Armadillo has ");
+System.out.print(74);
+System.out.println(" teeth.");
+System.out.println();
     // your code ends here!
   }
 }
