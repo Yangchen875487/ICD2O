6a3ad9d7320cd849afdcecdbcd17ea5eccc4c1eb
Yangchen875487 <875487@pdsb.net>
2024-09-19 23:01:19 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -6,7 +6,11 @@ class myo_random [class Testing {
      System.out.println("The number of keys on a keyboard is");
   System.out.println(102);
   System.out.println("The value of pi is");
-  System.out.println(3.14159);
+  System.out.println("double pi");
+  double circ;
+  double rad = 0.5;
+  System.out.println("The circumference of a circle of radius 1.5 is");
+  double pi = 3.14;
     // your code ends here!
   }
 }
