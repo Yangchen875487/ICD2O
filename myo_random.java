13ac4b2d430de7ffed148c147b21cf50cd4e7bcf
Yangchen875487 <875487@pdsb.net>
2024-09-20 12:38:57 -0400

    Update myo_random.java

--- myo_random.java
+++ myo_random.java
@@ -11,6 +11,12 @@ class myo_random [class Testing {
   double rad = 0.5;
   System.out.println("The circumference of a circle of radius 1.5 is");
   double pi = 3.14;
+    System.out.println("Welcome to Sanjay's Koala Party!");
+System.out.print("It's possible Sanjay can distribute " + 12);
+System.out.println(" cookies for each bowl.");
+System.out.print("Sanjay has "+ 6 + " for " + 144 + " cookies.");
+System.out.println("The number of cookies per bowl is");
+System.out.println ("The numbers of cookies per bowl is: " + (144/6));
     // your code ends here!
   }
 }
